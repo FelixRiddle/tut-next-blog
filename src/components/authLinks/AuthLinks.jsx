2794e7ddb96faa5ad2c614aab5b15fb09a590d6f
Felix <fedecraft3312@gmail.com>
2024-04-05 08:42:23 -0300

    Did the navbar for sizes, but it seems I've missed something

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./authLinks.module.css";
+import { useState } from "react";
 
 /**
  * 
@@ -7,16 +10,42 @@ import styles from "./authLinks.module.css";
  */
 export default async function AuthLinks() {
     const status = "notauthenticated";
+    const [open, setOpen] = useState(false);
     
     return (
         <>
+            {/* Check if the user is authenticated */}
             {status === "notauthenticated" ? (
-                <Link href="/login">Login</Link>
+                <Link href="/login" className={styles.link}>Login</Link>
             ) : (
                 <>
-                    <Link href="/write">Write</Link>
+                    <Link href="/write" className={styles.link}>Write</Link>
                     <span className={styles.link}>Logout</span>
                 </>
+            )}
+            
+            <div className={styles.burger} onClick={() => setOpen(!open)}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+            
+            {open && (
+                <div className={styles.responsiveMenu}>
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">About</Link>
+                    <Link href="/">Contact</Link>
+                    
+                    {/* Check if the user is authenticated */}
+                    {status === "notauthenticated" ? (
+                        <Link href="/login">Login</Link>
+                    ) : (
+                        <>
+                            <Link href="/write">Write</Link>
+                            <span className={styles.link}>Logout</span>
+                        </>
+                    )}
+                </div>
             )}
         </>
     );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,9 +19,9 @@ const Navbar = () => {
             <div className={styles.logo}>Blog of the worlds</div>
             <div className={styles.links}>
                 <ThemeToggle />
-                <Link href="/">Homepage</Link>
-                <Link href="/contact">Contact</Link>
-                <Link href="/about">About</Link>
+                <Link className={styles.link} href="/">Homepage</Link>
+                <Link className={styles.link} href="/contact">Contact</Link>
+                <Link className={styles.link} href="/about">About</Link>
             </div>
         </div>
     )
